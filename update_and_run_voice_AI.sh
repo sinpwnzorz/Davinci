7b66534d0da27792cad8c181befd2cb895fc4b55
sinpwnzorz <sinpwnzorz@gmail.com>
2021-01-10 02:06:20 -0700

    Delete update_and_run_voice_AI.sh

--- update_and_run_voice_AI.sh
+++
@@ -1,3 +0,0 @@
-rsync -avzhe ssh root@freenas.lianowarlegion.com:/mnt/mainpool/nas/windows/Git/deepspeech/mic_vad_streaming.py ~/dspeech/DeepSpeech-examples/mic_vad_streaming
-cd dspeech/
-python3 DeepSpeech-examples/mic_vad_streaming/mic_vad_streaming.py -m deepspeech-0.9.3-models.tflite -s deepspeech-0.9.3-models.scorer -r 44100
